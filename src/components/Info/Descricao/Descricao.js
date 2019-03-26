bab8ac2f222900cff1a75f000bb5bd104347075a
felipecarvalho180 <felipecarvalho180@gmail.com>
2019-03-26 02:57:25 -0300

    dia 1

--- src/components/Info/Descricao/Descricao.js
+++ src/components/Info/Descricao/Descricao.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import RedesSociais from './RedesSociais/RedesSociais';
+import foto from '../../../assets/foto.jpg'
+import classes from './Descricao.css';
 
 const descricao = (props) => {
     return(
-        <Auxiliary>
-            <h1>Felipe Carvalho</h1>
-            <h4>Desenvolvedor Web</h4>
-            <p>Olá eu sou Felipe Carvalho. Estudo Desenvolvimento Web a 1 ano, estudando varios elementos
-                do Desenvolvimento Web como HTML, CSS, JavaScript, SASS, React, JQuery e Bootstrap. Tive breve
-                contato com C++ na faculdade e Java durante um periodo que estudava Android.
-            </p>
-            <p>Idade: 21</p>
-            <p>Endereço: Siméria, Petrópolis-RJ, Brasil</p>
-            <p>Email: felipecarvalho180@gmail.com</p>
-            <p>Telefone: (24) 992560137</p>
-            <p>Nacionalidade: Brasileiro</p>
-            <RedesSociais/>
-        </Auxiliary>
+        <div className={classes.Descri}>
+            <div>
+                <img src={foto} alt="foto" className={classes.Foto}/> <br/>
+                <button className={classes.Curriculo}>Baixar Curriculo</button>
+            </div>
+            <div className={classes.Dados}>
+                <p className={classes.Nome}>Felipe <span className={classes.Azul}>Carvalho</span><br/>
+                <span className={classes.Profissao}>Desenvolvedor Web</span></p>
+                <p>Olá eu sou Felipe Carvalho. Estudo Desenvolvimento Web a 1 ano, estudando varios elementos
+                    do Desenvolvimento Web como HTML, CSS, JavaScript, SASS, React, JQuery e Bootstrap. Tive breve
+                    contato com C++ na faculdade e Java durante um periodo que estudava Android.
+                </p>
+                <p>Idade: 21</p>
+                <p>Endereço: Siméria, Petrópolis-RJ, Brasil</p>
+                <p>Email: felipecarvalho180@gmail.com</p>
+                <p>Telefone: (24) 992560137</p>
+                <p>Nacionalidade: Brasileiro</p>
+                <RedesSociais/>
+            </div>
+        </div>
     );
 }
 
--- src/components/Info/Descricao/RedesSociais/RedesSociais.js
+++ src/components/Info/Descricao/RedesSociais/RedesSociais.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import insta from '../../../../assets/instagram-logo.png';
+
 import classes from './RedesSociais.css';
 
 const redesSociais = (props) => (
         <div className={classes.Redes}>
-            <button>
-                <img src={insta} alt="insta"/>
-            </button>
-            <button>
-                <img src={insta} alt="insta"/>
-            </button>
+            <p>Redes Sociais:</p>
+            <button className={classes.Face}></button>
+            <button className={classes.Linkedin}></button>
+            <button className={classes.Insta}></button>
         </div>
 );
 
