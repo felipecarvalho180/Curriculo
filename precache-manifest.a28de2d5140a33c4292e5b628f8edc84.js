f3bdb5e286adb76a2385a59e3afb47bc340e2f13
felipecarvalho180 <felipecarvalho180@gmail.com>
2019-04-01 02:43:04 -0300

    Updates

---
+++ precache-manifest.a28de2d5140a33c4292e5b628f8edc84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37424448b77a5f9dd66522b4cc72b3a3",
+    "url": "/myapp/static/media/foto.37424448.jpg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "ec68fca86ce1f7323f2a",
+    "url": "/myapp/static/js/main.c3da9a98.chunk.js"
+  },
+  {
+    "revision": "19febef5f1cf936fc55f",
+    "url": "/myapp/static/js/2.c2721462.chunk.js"
+  },
+  {
+    "revision": "ec68fca86ce1f7323f2a",
+    "url": "/myapp/static/css/main.b63d9d77.chunk.css"
+  },
+  {
+    "revision": "f9c7cdb121ff5eeac4d9307f700335e1",
+    "url": "/myapp/index.html"
+  }
+];
