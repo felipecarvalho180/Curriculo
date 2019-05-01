b7810ae215e434baf9febf896afe4508d670af8c
Felipe <felipecarvalho180@gmail.com>
2019-05-01 06:45:59 -0300

    Updates

---
+++ precache-manifest.d079dbdf46a151e4a6cf08ad86fd2d72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "366053eb035fd35141ee8d94c0742360",
+    "url": "/Curriculo/static/media/img003.366053eb.jpg"
+  },
+  {
+    "revision": "683c2c8fe754356299e2",
+    "url": "/Curriculo/static/css/main.d1159318.chunk.css"
+  },
+  {
+    "revision": "fa8696e4e3ec336dffce",
+    "url": "/Curriculo/static/js/runtime~main.77597c98.js"
+  },
+  {
+    "revision": "49830dfb307d168ff641",
+    "url": "/Curriculo/static/js/2.9cf6a4be.chunk.js"
+  },
+  {
+    "revision": "37424448b77a5f9dd66522b4cc72b3a3",
+    "url": "/Curriculo/static/media/foto.37424448.jpg"
+  },
+  {
+    "revision": "399888ec5d8f2c0034ef7a87f72cfa56",
+    "url": "/Curriculo/static/media/img.399888ec.jpg"
+  },
+  {
+    "revision": "c5ab1d545ae33019bdde498071dd2227",
+    "url": "/Curriculo/static/media/img002.c5ab1d54.jpg"
+  },
+  {
+    "revision": "683c2c8fe754356299e2",
+    "url": "/Curriculo/static/js/main.f844cd1b.chunk.js"
+  },
+  {
+    "revision": "1a164e4664c8a5476860668d26e9ada9",
+    "url": "/Curriculo/static/media/img004.1a164e46.jpg"
+  },
+  {
+    "revision": "e23fb33207cb24d142959fb1ef4e3113",
+    "url": "/Curriculo/static/media/img006.e23fb332.jpg"
+  },
+  {
+    "revision": "1670a5327760321529b1ab3f398b8e71",
+    "url": "/Curriculo/static/media/img007.1670a532.jpg"
+  },
+  {
+    "revision": "4eed0c6340f6e9e1cb6be98a679029bd",
+    "url": "/Curriculo/static/media/img008.4eed0c63.jpg"
+  },
+  {
+    "revision": "ad3f930987f44cfe78f0ec9ec6b90eca",
+    "url": "/Curriculo/static/media/img009.ad3f9309.jpg"
+  },
+  {
+    "revision": "2e6f4ebdfb4510a7a1bcb67b2e143f23",
+    "url": "/Curriculo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Curriculo/precache-manifest.4d10e2d597ee59af1df69106ac7ef73b.js"
+  "/Curriculo/precache-manifest.d079dbdf46a151e4a6cf08ad86fd2d72.js"
 );
 
 workbox.clientsClaim();
